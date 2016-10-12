9c52bd7eda8ede79931c4adb4cc00a77c17b45c6
Grant Wells <gwells4@cnm.edu>
2016-10-12 08:41:54 -0600

    Added initial index file.

---
+++ public_html/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8" />
+		<link rel="Browser Tab Icon" href="img/icon-php-file.jpg" type="image/jpg">
+
+		<!-- Add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
+
+		<title>Repository 3: About Me</title>
+
+	</head>
+
+	<body>
+
+		<!-- This is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- This is the main page content. -->
+		<main>
+
+			<!-- Begin section 1 -->
+			<section>
+				<p>Jean shorts biodiesel coloring book, skateboard farm-to-table tofu kombucha put a bird on it sartorial quinoa vexillologist authentic kitsch. Lo-fi pok pok mumblecore hell of put a bird on it, ramps squid before they sold out beard bushwick. Tacos hoodie tousled tumblr poutine. Hella DIY austin twee yr. Lomo kombucha tilde mlkshk forage flexitarian. Pok pok cray williamsburg direct trade unicorn vinyl viral. Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
+				<img src="img/sandbox-image-1.jpg" alt="First Sandbox Image">
+			</section>
+
+			<!-- Begin section 2 -->
+			<section>
+				<h2>My Favorite Things</h2>
+				<ul>
+					<li>Senator Arlo</li>
+					<li>Star Wars</li>
+					<li>Donuts</li>
+				</ul>
+			</section>
+
+			<!-- Begin section 3 -->
+			<section>
+
+				<table>
+
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Star Trek</td>
+					</tr>
+
+				</table>
+
+			</section>
+
+		</main>
+
+	</body>
+</html>
