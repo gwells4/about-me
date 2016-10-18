16c6e71305bfd1cc0c60622754c6b3d8c9a61d57
Grant Wells <gwells4@cnm.edu>
2016-10-18 08:50:47 -0600

    Updated about me site in preparation for PWP.

--- public_html/index.php
+++ public_html/index.php
@@ -4,12 +4,12 @@
 	<head>
 
 		<meta charset="utf-8" />
-		<link rel="Browser Tab Icon" href="img/icon-php-file.jpg" type="image/jpg">
+		<link rel="Browser-Tab Icon" href="img/icon-list.svg" type="image/svg">
 
 		<!-- Add CSS file -->
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
 
-		<title>Repository 3: About Me (PWP beta)</title>
+		<title>Repository 4: Data Design - Craigslist Events</title>
 
 	</head>
 
@@ -17,54 +17,133 @@
 
 		<!-- This is the header -->
 		<header>
-			<h1>Hi! This is my page.</h1>
+			<h1>Data Design: Craigslist Events</h1>
 		</header>
 
 		<!-- This is the main page content. -->
 		<main>
 
-			<!-- Begin section 1 -->
+			<!-- Section 1: Persona -->
 			<section>
-				<p>Knausgaard chia helvetica post-ironic, meggings schlitz lomo.</p>
-				<img src="img/sandbox-image-1.jpg" alt="First Sandbox Image">
+
+				<h2>Persona</h2>
+
+				<img id="picture-male-hipster" src="img/male-hipster.jpg" alt="Picture of Male Hipster for Persona">
+				<img id="picture-female-hipster" src="img/female-hipster-annotated.jpg" alt="Picture of Female Hipster for Persona">
+
+				<ol>
+					<li>
+						<b>Quote</b><br>
+						<i>"I can't believe I drank my coffee before it was cool."</i>
+					</li>
+					<li>
+						<b>Description</b><br>
+						<u>Hipster:</u> A person considered by themselves (and others) to be at the forefront of what's "hip". They may be either male, female, or other. The hip forefront generally includes fashion, and it may also include food &amp; drink, music, art, and travel destinations. One's neighborhood is also very important, as the hipster considers there to be a clear line between fashionable and unfashionable neighborhoods. (See: <a href="https://en.wikipedia.org/wiki/The_Hipster_Handbook"><i>The Hipster Handbook</i></a> for more details.)
+					</li>
+					<li>
+						<b>Needs</b> (of the User from the Site)<br>
+						The hipster needs the most up-to-date information to pick the most hip event to attend. Enough detail needs to be included to judge the hipness, so posters of events should be able to enter an adequate description and location, as well as the date &amp; time information.
+					</li>
+					<li>
+						<b>Frustrations</b><br>
+						The hipsters number one frustration is not having enough information to judge the hipness of an event. When in doubt, though, the hipster will rule out all events that do not include enough details for the judge the hipness of the event.
+					</li>
+					<li>
+						<b>Ideal Features</b><br>
+						Prompts for event posters to input detailed information, such as:
+						<ul>
+							<li>Date &amp; Time</li>
+							<li>Venue (Address or GPS)</li>
+							<li>Food? Non/organic? Vegetarian? Allergy-compatible?</li>
+							<li>Entertainment</li>
+							<li>Special Guests</li>
+							<li>Schedule</li>
+						</ul>
+					</li>
+					<li>
+						<b>Scenarios</b><br>
+						<ol>
+							<li>The fridge is empty, and the hipster needs to know where a food event may be going on.</li>
+							<li>The hipster is planning their evening activities. What's happening, tonight?</li>
+							<li>It's a lazy night, and the hipster doesn't want to drive. What's nearby?</li>
+						</ol>
+					</li>
+				</ol>
+
 			</section>
 
-			<!-- Begin section 2 -->
+			<!-- Section 2: Use Case -->
 			<section>
-				<h2>My Favorite Things</h2>
-				<ul>
-					<li>Senator Arlo</li>
-					<li>Star Wars</li>
-					<li>Donuts</li>
-				</ul>
+
+				<h2>Use Case</h2>
+
+				<p>The hipster will interact with the site based on the scenarios above. The flowchart below visually depicts the steps they will take while using the site.</p>
+
+				<img  id="diagram-use-case" src="img/about-me-use-case.svg" alt="Flow chart for use case" >
+
 			</section>
 
-			<!-- Begin section 3 -->
+			<!-- Section 3: User Interaction Flow -->
 			<section>
+
+				<h2>User Interaction Flow</h2>
+
+				<ol>
+					<li>If the user wants to know what events are on a specific date, then they search using the calendar.</li>
+					<li>If the user wants to know what events of a certain type are going on, then they will check the appropriate boxes for the category of the even they are interested in.</li>
+					<li>If the user wants to know what events are related to a specific keyword, then they will use the search box to search the events for those keywords.</li>
+				</ol>
+
+			</section>
+
+			<!-- Section 4: Entities -->
+			<section>
+
+				<h2>Data Design</h2>
+
+				<h3>Entities</h3>
 
 				<table>
-
 					<tr>
-						<th>Column One</th>
-						<th>Column Two</th>
+						<th>Entity</th>
+						<th>events</th>
+						<th>images</th>
+						<th>posters</th>
 					</tr>
-
 					<tr>
-						<td>Cat</td>
-						<td>Dog</td>
+						<th>Attributes</th>
+						<td>
+							eventId<br>
+							eventDateTime<br>
+							eventVenue<br>
+							eventCategory<br>
+							eventDetails
+						</td>
+						<td>
+							posterName<br>
+							posterEmail<br>
+							posterPhone<br>
+							posterEventId
+						</td>
+						<td>
+							imageId<br>
+							imageEventId<br>
+							imageFilename<br>
+							imagePath<br>
+						</td>
 					</tr>
-
-					<tr>
-						<td>Car</td>
-						<td>Bike</td>
-					</tr>
-
-					<tr>
-						<td>Star Wars</td>
-						<td>Star Trek</td>
-					</tr>
-
 				</table>
+
+				<h3>Conceptual Model</h3>
+
+				<ol>
+					<li>Many <b>events</b> can have many <b>images</b>.</li>
+					<li>One <b>poster</b> can post many <b>events</b>.</li>
+				</ol>
+
+				<h3>Entity Relationship Diagram</h3>
+
+				<img  id="diagram-entity-relationship" src="img/about-me-erd.svg" alt="Entity Relationship Diagram" >
 
 			</section>
 
